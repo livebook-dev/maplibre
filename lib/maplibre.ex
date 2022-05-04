0ad6a2f27fe9836241d635df865c843dcfe96f69
Cristine Guadelupe <cristineguadelupe@me.com>
2022-05-04 19:00:04 -0300

    Initial commit

---
+++ lib/maplibre.ex
@@ -0,0 +1,18 @@
+defmodule Maplibre do
+  @moduledoc """
+  Documentation for `Maplibre`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Maplibre.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/maplibre_test.exs
@@ -0,0 +1,8 @@
+defmodule MaplibreTest do
+  use ExUnit.Case
+  doctest Maplibre
+
+  test "greets the world" do
+    assert Maplibre.hello() == :world
+  end
+end
