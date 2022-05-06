5b75d767960b070a695561b6dabf4887d2fd5ef3
José Valim <jose.valim@gmail.com>
2022-05-06 12:47:40 +0200

    Update maplibre.ex

--- lib/maplibre.ex
+++ lib/maplibre.ex
@@ -63,7 +63,7 @@ defmodule Maplibre do
 
   ## Options
 
-    Only the following properties are allowed directly on `new/1`
+  Only the following properties are allowed directly on `new/1`
 
     * `:bearing` -  Default bearing, in degrees. The bearing is the compass direction that is
       "up"; for example, a bearing of 90° orients the map so that east is up. This value will be
@@ -87,11 +87,12 @@ defmodule Maplibre do
     * `:style` - The initial style specification. Default:
       "https://demotiles.maplibre.org/style.json"
 
-    To manipulate any other [style root
-    properties](https://maplibre.org/maplibre-gl-js-docs/style-spec/root/), use the
-    corresponding functions
+  To manipulate any other [style root
+  properties](https://maplibre.org/maplibre-gl-js-docs/style-spec/root/), use the
+  corresponding functions
 
   ## Examples
+
       Ml.new(
         center: {-74.5, 40},
         zoom: 9,
@@ -140,6 +141,7 @@ defmodule Maplibre do
   property, which must be one of `:vector`, `:raster`, `:raster_dem`, `:geojson`, `:image` or `:video`.
 
   ## Examples
+
       |> Ml.add_source("rwanda-provinces",
             type: :geojson,
             data: "https://maplibre.org/maplibre-gl-js-docs/assets/rwanda-provinces.geojson"
@@ -186,10 +188,7 @@ defmodule Maplibre do
 
     * `:id` - Unique layer name.
 
-    * `:type` - One of `:fill`, `:line`, `:symbol`, `:circle`, `:heatmap` `:fill_extrusion`,
-      `:raster`, `:hillshade` or `:background`.
-
-      ### Rendering type of this layer.
+    * `:type` - One of:
 
       * `:fill` -  A filled polygon with an optional stroked border.
       * `:line` -  A stroked line.
